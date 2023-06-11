082a88fdb49de8c0d81c4d94c7d57895f38ea27b
FValent3 <fpvrodrigues@gmail.com>
2023-06-11 18:59:29 -0300

    fix: fix movies model

--- src/models/movies.js
+++ src/models/movies.js
@@ -2,10 +2,10 @@ import Parse from 'parse/node'
 
 const Movies = Parse.Object.extend('Movie', {
   initialize: function (attrs, options) {
-    this.set('title', attrs.title)
-    this.set('description', attrs.description)
-    this.set('poster', attrs.poster)
-    this.set('releaseYear', attrs.releaseYear)
+    this.title = attrs.title
+    this.description = attrs.description
+    this.poster = attrs.poster
+    this.releaseYear = attrs.releaseYear
   }
 })
 
